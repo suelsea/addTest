ab8652095707723c7c04614d6b9058972475c8ca
suelsea <plasil2009@hotmail.com>
2013-06-24 22:14:18 +0800

    2

--- www/js/app.k169xY31oS.js
+++ www/js/app.k169xY31oS.js
@@ -11662,7 +11662,7 @@ Blu.fn.UI.showGallery = function() {
         
         $('#open-login').click( function(){
             //alert('hdjhdhj');
-            window.plugins.childBrowser.showWebPage(_urlLogin, { showLocationBar: false, showNavigationBar: false });
+            window.plugins.childBrowser.showWebPage(_urlLogin, { showLocationBar: true, showNavigationBar: true });
 
 
             window.plugins.childBrowser.onClose = function () {
